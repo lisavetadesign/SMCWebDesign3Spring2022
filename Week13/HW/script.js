51d9ebc02a7fcbcf923779487c29d462873df0b2
lisavetadesign <79555711+lisavetadesign@users.noreply.github.com>
2022-05-30 16:46:17 -0700

    BM Structure

---
+++ Week13/HW/script.js
@@ -0,0 +1,15 @@
+var sectionScroll = document.getElementById("scrollSection")
+
+var elemOne = document.getElementById("scrollElement1");
+var elemTwo = document.getElementById("scrollElement2");
+var elemThree = document.getElementById("scrollElement3")
+
+//what do I need to say?
+//Stop (focus) on scroll section
+//Stay on scroll section
+//Scroll through elem1-elem3
+//move on to the rest of the document
+
+
+
+sectionScroll.addEventListener("scroll",function(event){console.log(event)})
